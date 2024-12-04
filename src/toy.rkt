8eee05cf3f141afc39b7e6ec8ba7e760cfbd5a57
Samuel Gélineau <gelisam+github@gmail.com>
2024-12-03 21:52:56 -0500

    day 2b

--- src/toy.rkt
+++ src/toy.rkt
@@ -7,12 +7,12 @@
   input
   (list "7 6 4 2 1" "1 2 7 8 9" "9 7 6 2 1" "1 3 2 4 5" "8 6 4 4 1" "1 3 6 7 9"))
 
-;; use the real input and disable the asserts
-;(set! input (file->lines "full-input.txt"))
-;(define (check-equal? _x _y)
-;  (void))
-;(define (check-true _)
-;  (void))
+; use the real input and disable the asserts
+(set! input (file->lines "full-input.txt"))
+(define (check-equal? _x _y)
+  (void))
+(define (check-true _)
+  (void))
 
 (define (parse-line line)
   (let ([parts (string-split line)])
@@ -24,41 +24,47 @@
 (define reports
   (map parse-line input))
 
-(define (increasing? xs)
-  (cond
-    [(null? xs)
-     #t]
-    [(null? (cdr xs))
-     #t]
-    [(<= (car xs) (cadr xs))
-     (increasing? (cdr xs))]
-    [else #f]))
-(check-true (increasing? (list 1 2 7 8 9)))
-(check-true (not (increasing? (list 1 2 8 7 9))))
-
-(define (decreasing? xs)
-  (increasing? (reverse xs)))
-(check-true (decreasing? (list 9 7 6 2 1)))
-(check-true (not (decreasing? (list 1 2 7 8 9))))
-
-(define (differences xs)
-  (apply map
-         (lambda (x y)
-           (abs (- x y)))
-         (list (cdr xs)
-               (drop-right xs 1))))
-(check-equal?
-  (differences (list 1 2 7 8 9))
-  (list 1 5 1 1))
+(define (safe-step? up? prev x)
+  (let ([down? (not up?)]
+        [diff (abs (- x prev))])
+    (and (or (and up? (< prev x))
+             (and down? (> prev x)))
+         (>= diff 1)
+         (<= diff 3))))
+(check-equal? (safe-step? #t 0 0) #f)
+(check-equal? (safe-step? #t 1 2) #t)
+(check-equal? (safe-step? #t 1 4) #t)
+(check-equal? (safe-step? #t 1 5) #f)
+(check-equal? (safe-step? #f 0 0) #f)
+(check-equal? (safe-step? #f 1 2) #f)
+(check-equal? (safe-step? #f 4 1) #t)
+(check-equal? (safe-step? #f 5 1) #f)
 
 (define (safe? xs)
-  (let ([diffs (differences xs)])
-    (and (or (increasing? xs)
-            (decreasing? xs))
-         (>= (apply min diffs) 1)
-         (<= (apply max diffs) 3))))
-(check-equal?
-  (map safe? reports)
-  (list #t #f #f #f #f #t))
+  (define n (length xs))
+  (define memo (make-hash))
+  (define (safe-from? dampener? up? prev i)
+    (hash-ref! memo (list dampener? up? prev i)
+      (λ ()
+        (if (= i n)
+            #t
+            (let ([x (list-ref xs i)]
+                  [next-i (+ i 1)])
+              (or (and (safe-step? up? prev x)
+                       (safe-from? dampener? up? x next-i))
+                  (and dampener?
+                       (safe-from? #f up? prev next-i))))))))
+  (let ([x0 (list-ref xs 0)]
+        [x1 (list-ref xs 1)]
+        [x2 (list-ref xs 2)])
+    (or (safe-from? #t (< x0 x1) x0 1)
+        (safe-from? #f (< x0 x2) x0 2)
+        (safe-from? #f (< x1 x2) x1 2))))
+(check-equal? (safe? (list 7 6 4 2 1)) #t)
+(check-equal? (safe? (list 1 2 7 8 9)) #f)
+(check-equal? (safe? (list 9 7 6 2 1)) #f)
+(check-equal? (safe? (list 1 3 2 4 5)) #t)
+(check-equal? (safe? (list 8 6 4 4 1)) #t)
+(check-equal? (safe? (list 1 3 6 7 9)) #t)
 
 (length (filter safe? reports))
