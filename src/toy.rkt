5af8b6e0cf9fb2bf88932eaefc0fce073e012d54
Samuel Gélineau <gelisam+github@gmail.com>
2024-12-04 23:38:36 -0500

    day 3a

--- src/toy.rkt
+++ src/toy.rkt
@@ -5,66 +5,65 @@
 (define input (file->lines "example-input.txt"))
 (check-equal?
   input
-  (list "7 6 4 2 1" "1 2 7 8 9" "9 7 6 2 1" "1 3 2 4 5" "8 6 4 4 1" "1 3 6 7 9"))
+  (list "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"))
 
-; use the real input and disable the asserts
-(set! input (file->lines "full-input.txt"))
-(define (check-equal? _x _y)
-  (void))
-(define (check-true _)
-  (void))
+;; use the real input and disable the asserts
+;(set! input (file->lines "full-input.txt"))
+;(define (check-equal? _x _y)
+;  (void))
+;(define (check-true _)
+;  (void))
+
+(define (string-drop s n)
+  (substring s n))
+(check-equal? (string-drop "abcdef" 3) "def")
+
+(define (string-drop-prefix? s prefix)
+  (and (string-prefix? s prefix)
+       (string-drop s (string-length prefix))))
+(check-equal? (string-drop-prefix? "abcdef" "abc") "def")
+(check-equal? (string-drop-prefix? "abcdef" "def") #f)
+
+(define (string-span pred s)
+  (let loop ([i 0])
+    (if (>= i (string-length s))
+        (list s "")
+        (if (pred (string-ref s i))
+            (loop (+ i 1))
+            (list (substring s 0 i) (substring s i))))))
+(check-equal? (string-span char-alphabetic? "abc123") (list "abc" "123"))
 
 (define (parse-line line)
-  (let ([parts (string-split line)])
-    (map string->number parts)))
+  (define (try-next-char)
+    (parse-line (string-drop line 1)))
+  (if (equal? line "")
+      (list)
+      (match (string-drop-prefix? line "mul(")
+        [(? string? rest)
+         (match (string-span char-numeric? rest)
+           [(list numeric-chars rest)
+            (if (non-empty-string? numeric-chars)
+                (let ([x (string->number numeric-chars)])
+                  (match (string-drop-prefix? rest ",")
+                    [(? string? rest)
+                     (match (string-span char-numeric? rest)
+                       [(list numeric-chars rest)
+                        (if (non-empty-string? numeric-chars)
+                            (let ([y (string->number numeric-chars)])
+                              (match (string-drop-prefix? rest ")")
+                                [(? string? rest)
+                                 (cons (list x y) (parse-line rest))]
+                                [_ (try-next-char)]))
+                            (try-next-char))])]
+                    [_ (try-next-char)]))
+                (try-next-char))])]
+        [_ (try-next-char)])))
 (check-equal?
-  (parse-line "7 6 4 2 1")
-  (list 7 6 4 2 1))
+  (parse-line "xxxmul(2,4)xxxmul(mul(2mul(2,mul(2,4mul(21,41)xxx")
+  (list (list 2 4) (list 21 41)))
 
-(define reports
-  (map parse-line input))
-
-(define (safe-step? up? prev x)
-  (let ([down? (not up?)]
-        [diff (abs (- x prev))])
-    (and (or (and up? (< prev x))
-             (and down? (> prev x)))
-         (>= diff 1)
-         (<= diff 3))))
-(check-equal? (safe-step? #t 0 0) #f)
-(check-equal? (safe-step? #t 1 2) #t)
-(check-equal? (safe-step? #t 1 4) #t)
-(check-equal? (safe-step? #t 1 5) #f)
-(check-equal? (safe-step? #f 0 0) #f)
-(check-equal? (safe-step? #f 1 2) #f)
-(check-equal? (safe-step? #f 4 1) #t)
-(check-equal? (safe-step? #f 5 1) #f)
-
-(define (safe? xs)
-  (define n (length xs))
-  (define memo (make-hash))
-  (define (safe-from? dampener? up? prev i)
-    (hash-ref! memo (list dampener? up? prev i)
-      (λ ()
-        (if (= i n)
-            #t
-            (let ([x (list-ref xs i)]
-                  [next-i (+ i 1)])
-              (or (and (safe-step? up? prev x)
-                       (safe-from? dampener? up? x next-i))
-                  (and dampener?
-                       (safe-from? #f up? prev next-i))))))))
-  (let ([x0 (list-ref xs 0)]
-        [x1 (list-ref xs 1)]
-        [x2 (list-ref xs 2)])
-    (or (safe-from? #t (< x0 x1) x0 1)
-        (safe-from? #f (< x0 x2) x0 2)
-        (safe-from? #f (< x1 x2) x1 2))))
-(check-equal? (safe? (list 7 6 4 2 1)) #t)
-(check-equal? (safe? (list 1 2 7 8 9)) #f)
-(check-equal? (safe? (list 9 7 6 2 1)) #f)
-(check-equal? (safe? (list 1 3 2 4 5)) #t)
-(check-equal? (safe? (list 8 6 4 4 1)) #t)
-(check-equal? (safe? (list 1 3 6 7 9)) #t)
-
-(length (filter safe? reports))
+(define output
+  (for/sum ([line input])
+    (for/sum ([pair (parse-line line)])
+      (apply * pair))))
+output
